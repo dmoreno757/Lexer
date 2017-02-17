cd953388cc77748a9dc17a6195753c292271cfd7
RJ <Esquiviasrj@gmail.com>
2017-02-16 17:49:32 -0800

    added second half of code with id's
    Still needs to be merged together

--- Source.cpp
+++ Source.cpp
@@ -160,3 +160,210 @@ int advance(char input, int arr[][numChars], int currentState)
 //STEP 1 Write Regular Expressions for all tokens we need to recognize
 //STEP 2 Form NFA using Thompson's construction that recognizes these tokens
 //Step 3 From the NFA, the subset construction will give the DFA which can be embedded as a table in the lexical analyzer
+
+//BELOW IS CODE THAT NEEDS TO BE MERGED INTO PRECEEDING CODE
+/*
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <locale>
+
+using namespace std;
+
+void output();
+int lineNum = 1;
+string define;
+
+string getChar();
+int ID;
+
+struct lex {
+
+};
+
+
+int main()
+{
+	ifstream inFile;
+	inFile.open("lexerExamp.txt");
+	while (getline(inFile, define))
+	{
+		getChar();
+		lineNum++;
+	}
+	return 0;
+}
+
+
+void output()
+{
+	/*cout << "(:Token " << lineNum << keyWD << ")" << endl;
+	if (option == 3)
+	{
+	cout << ":ix " << ixNum;
+	}
+	else if (option == 2)
+	{
+	cout << ":str" << "" << define << "" << ")";
+	}
+	*/
+}
+
+string getChar()
+{
+
+}
+
+
+void lexerLine(string str)
+{
+	string token;
+	stringstream stringStream(str);
+	bool commentFound;
+	while (stringStream >> token && !commentFound)
+	{
+		//token[0]
+		if (isalpha(str[0]))
+			//void
+			alphAFunc(token);
+		else if (isdigit(str[0]))
+			//void
+			digitFunc(token);
+		else 
+			//void
+			commentFound = symbolFunc(token);
+			
+	}
+}
+
+
+void alphAFunc(string token)
+{
+
+}
+
+
+bool symbolFunc(string token)
+{
+	// return if comment found
+}
+
+void digitFunc(string token) 
+{
+	if (isdigit[token])
+	{
+		setID();
+	}
+	else if (token == '.')
+	{
+		setID();
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+
+void setID(string y, string orange)
+{
+	if (y == "identifier")
+		ID = 2;
+	else if (y = "integer")
+		ID = 3;
+	else if (y = "float")
+		ID = 4;
+	else if (y = "string")
+		ID = 5;
+	//Unpaired delimiters
+	else if (orange = ",")
+		ID = 6;
+	else if (orange = ";")
+		ID = 7;
+	//keywords
+	else if (orange = "prog")
+		ID = 10;
+	else if (orange = "main")
+		ID = 11;
+	else if (orange = "fcn")
+		ID = 12;
+	else if (orange = "class")
+		ID = 13;
+	else if (orange = "float")
+		ID = 15;
+	else if (orange = "int")
+		ID = 16;
+	else if (orange = "string")
+		ID = 17;
+	else if (orange = "if")
+		ID = 18;
+	else if (orange = "elseif")
+		ID = 19;
+	else if (orange = "else")
+		ID = 20;
+	else if (orange = "while")
+		ID = 21;
+	else if (orange = "input")
+		ID = 22;
+	else if (orange = "print")
+		ID = 23;
+	else if (orange = "new")
+		ID = 24;
+	else if (orange = "return")
+		ID = 25;
+	// Paired delieters
+	else if (orange = "<")
+		ID = 31;
+	else if (orange = ">")
+		ID = 32;
+	else if (orange = "{")
+		ID = 33;
+	else if (orange = "}")
+		ID = 34;
+	else if (orange = "[")
+		ID = 35;
+	else if (orange = "]")
+		ID = 35;
+	else if (orange = "(")
+		ID = 37;
+	else if (orange = ")")
+		ID = 38;
+	//Other punctuation
+	else if (orange = "*")
+		ID = 41;
+	else if (orange = "^")
+		ID = 42;
+	else if (orange = ":")
+		ID = 43;
+	else if (orange = ".")
+		ID = 44;
+	else if (orange = "=")
+		ID = 45;
+	else if (orange = "-")
+		ID = 46;
+	else if (orange = "+")
+		ID = 47;
+	else if (orange = "/")
+		ID = 48;
+	//Multi-char operators
+	else if (orange = "->")
+		ID = 51;
+	else if (orange = "==")
+		ID = 52;
+	else if (orange = "!=")
+		ID = 53;
+	else if (orange = "<=")
+		ID = 54;
+	else if (orange = ">=")
+		ID = 55;
+	else if (orange = "<<")
+		ID = 56;
+	else if (orange = ">>")
+		ID = 57;
+	//miscellaeous
+	else if (orange = "error")
+		ID = 99;
+}
+*/
